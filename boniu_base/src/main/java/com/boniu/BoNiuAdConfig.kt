a758d37aaca53afa66ef35626a48689c8e0de49c
just_coder <meng.huang@archermind.com>
2021-05-21 10:12:37 +0800

    去掉广告依赖

--- boniu_base/src/main/java/com/boniu/BoNiuAdConfig.kt
+++ boniu_base/src/main/java/com/boniu/BoNiuAdConfig.kt
@@ -1,11 +1,5 @@
 package com.boniu
 
-import android.content.Context
-import com.bytedance.sdk.openadsdk.TTAdConfig
-import com.bytedance.sdk.openadsdk.TTAdConstant
-import com.bytedance.sdk.openadsdk.TTAdSdk
-import com.qq.e.comm.managers.GDTADManager
-
 /**
  * 博牛广告配置
  */
@@ -20,7 +14,6 @@ object BoNiuAdConfig {
 
 
     fun init(
-        context: Context,
         ttAdAppId: String, ttAdSplashCodeId: String, ttAdUrgeCodeId: String,
         gdtAdAppId: String, gdtAdSplashCodeId: String, gdtAdUrgeCodeId: String
     ) {
@@ -31,31 +24,6 @@ object BoNiuAdConfig {
         this.gdtAdSplashCodeId = gdtAdSplashCodeId
         this.gdtAdUrgeCodeId = gdtAdUrgeCodeId
 
-        initTTAd(context)
     }
 
-    private fun initTTAd(context: Context) {
-        val config = TTAdConfig.Builder()
-            .appId(this.ttAdAppId)
-            .useTextureView(true)
-            .appName(BoNiuConfig.getAppName())
-            .titleBarTheme(TTAdConstant.TITLE_BAR_THEME_DARK)
-            .allowShowNotify(true) //是否允许sdk展示通知栏提示
-            .allowShowPageWhenScreenLock(true) //是否在锁屏场景支持展示广告落地页
-            .debug(BuildConfig.DEBUG) //测试阶段打开，可以通过日志排查问题，上线时去除该调用
-            .directDownloadNetworkType(
-                TTAdConstant.NETWORK_STATE_WIFI,
-                TTAdConstant.NETWORK_STATE_3G
-            ) //允许直接下载的网络状态集合
-            .supportMultiProcess(false)//是否支持多进程
-            .build()
-        TTAdSdk.init(context, config)
-    }
-
-    /**
-     * 初始化广点通
-     */
-    private fun initGDTAd(context: Context) {
-        GDTADManager.getInstance().initWith(context, this.gdtAdAppId)
-    }
 }
