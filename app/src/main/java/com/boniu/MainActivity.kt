ba8cb31bce4a6edcc41368efd9baca4a39c285cd
just_coder <meng.huang@archermind.com>
2021-06-07 17:34:35 +0800

    去掉不用得东西

--- app/src/main/java/com/boniu/MainActivity.kt
+++ app/src/main/java/com/boniu/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.boniu
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
 }
