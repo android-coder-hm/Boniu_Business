e95edb013b2030c00c15b307960bb81431430ef8
just_coder <meng.huang@archermind.com>
2021-06-07 17:15:06 +0800

    更新版本

--- boniu_base/src/main/java/com/boniu/UserHelper.kt
+++ boniu_base/src/main/java/com/boniu/UserHelper.kt
@@ -1,13 +1,10 @@
 package com.boniu
 
-import com.adk.getShareValue
-import com.adk.putShareValue
+import com.adk.getShare
+import com.adk.putShare
 import com.boniu.http.UserAccountInfoResBean
 import com.google.gson.Gson
 import java.util.*
-
-//通用的share文件
-private const val SHARE_COMMON_FILE = "shareCommonFile"
 
 //是否是第一次打开应用
 private const val SHARE_KEY_IS_FIRST_OPEN = "isFirstOpen"
@@ -26,35 +23,34 @@ private const val SHARE_KEY_LOGIN_ACCOUNT_INFO = "loginAccountInfo"
 
 
 /*************    是否是第一次打开应用        *****************/
-fun isFirstOpen() = getShareValue(SHARE_COMMON_FILE, SHARE_KEY_IS_FIRST_OPEN, true)
+fun isFirstOpen() = getShare(SHARE_KEY_IS_FIRST_OPEN, true)
 
 fun updateIsFirstOpen() {
-    putShareValue(SHARE_COMMON_FILE, SHARE_KEY_IS_FIRST_OPEN, false)
+    putShare(SHARE_KEY_IS_FIRST_OPEN, false)
 }
 
 /*************    是否已经申请注销账号        *****************/
-fun haveCancelAccount() = getShareValue(SHARE_COMMON_FILE, SHARE_KEY_IS_CANCEL_TOKEN, false)
+fun haveCancelAccount() = getShare(SHARE_KEY_IS_CANCEL_TOKEN, false)
 
 fun setCancelAccount() {
-    putShareValue(SHARE_COMMON_FILE, SHARE_KEY_IS_CANCEL_TOKEN, true)
+    putShare(SHARE_KEY_IS_CANCEL_TOKEN, true)
 }
 
 
 /***************  获取AccountId    *******************/
-fun getLoginAccountId() = getShareValue(SHARE_COMMON_FILE, SHARE_KEY_ACCOUNT_ID, "")
+fun getLoginAccountId() = getShare(SHARE_KEY_ACCOUNT_ID, "")
 
 fun saveLoginAccountId(accountId: String) {
-    putShareValue(
-        SHARE_COMMON_FILE,
+    putShare(
         SHARE_KEY_ACCOUNT_ID, accountId
     )
 }
 
 /***************     获取TOKEN      *******************/
-fun getToken() = getShareValue(SHARE_COMMON_FILE, SHARE_KEY_TOKEN, "")
+fun getToken() = getShare(SHARE_KEY_TOKEN, "")
 
 fun saveToken(token: String) {
-    putShareValue(SHARE_COMMON_FILE, SHARE_KEY_TOKEN, token)
+    putShare(SHARE_KEY_TOKEN, token)
 }
 
 
@@ -62,24 +58,22 @@ fun saveToken(token: String) {
  * 获取登录的账户信息
  */
 fun getLoginAccountInfo(): UserAccountInfoResBean {
-    val data = getShareValue(SHARE_COMMON_FILE, SHARE_KEY_LOGIN_ACCOUNT_INFO, "")
+    val data = getShare(SHARE_KEY_LOGIN_ACCOUNT_INFO, "")
     return Gson().fromJson(data, UserAccountInfoResBean::class.java)
 }
 
 fun saveLoginAccountInfo(userAccountInfoResBean: UserAccountInfoResBean?) {
     if (null == userAccountInfoResBean) {
-        putShareValue(SHARE_COMMON_FILE, SHARE_KEY_LOGIN_ACCOUNT_INFO, "")
+        putShare(SHARE_KEY_LOGIN_ACCOUNT_INFO, "")
     } else {
-        putShareValue(
-            SHARE_COMMON_FILE, SHARE_KEY_LOGIN_ACCOUNT_INFO, Gson().toJson(userAccountInfoResBean)
-        )
+        putShare(SHARE_KEY_LOGIN_ACCOUNT_INFO, Gson().toJson(userAccountInfoResBean))
     }
 }
 
 /**
  * 是否已经登录
  */
-fun haveLogin() = getShareValue(SHARE_COMMON_FILE, SHARE_KEY_LOGIN_ACCOUNT_INFO, "").isNotEmpty()
+fun haveLogin() = getShare(SHARE_KEY_LOGIN_ACCOUNT_INFO, "").isNotEmpty()
 
 /**
  * 清楚本地存储的登录信息
